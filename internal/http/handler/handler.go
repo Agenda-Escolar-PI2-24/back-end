f22f57e5eaf48fd8629f008228f40fecc2571f91
Ataíde <welitimbtos@yahoo.com.br>
2024-11-14 15:31:09 -0300

    add health check

--- internal/http/handler/handler.go
+++ internal/http/handler/handler.go
@@ -3,13 +3,19 @@ package handler
 import (
 	"agenda-escolar/internal/http/controller"
 	"agenda-escolar/pkg"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 var agendaController controller.TaskController
 
+func healthCheck(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "OK"})
+}
+
 func HandleRequests(router *gin.Engine) {
+	router.GET("/", healthCheck)
 	v1 := router.Group("/v1")
 
 	authRoutes := v1.Group("/auth")
